a9380183fcd4b2f9fb8cce2d9408f3b1fdd31fe8
kh14762 <kevin.j.heritage@gmail.com>
2026-07-26 23:19:24 -0400

    added themes to nvim and alacritty

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,27 @@
 return {
-  { "AlexvZyl/nordic.nvim" },
+  -- "ellisonleao/gruvbox.nvim",
+  -- config = function()
+  --   require("gruvbox").setup({
+  --     contrast = "hard", -- Set contrast variant to hard
+  --     -- Additional options can be added here
+  --   })
+  --   vim.opt.background = "dark"
+  --   vim.cmd.colorscheme("gruvbox")
+  -- end,
 
-  {
-    "Lazyvim/Lazyvim",
-    opts = {
-      colorscheme = "nordic",
-    },
-  },
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  config = function()
+    -- Configuration options must be set before loading the colorscheme
+
+    vim.g.gruvbox_material_foreground = "original"
+    vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_enable_italic = 1
+
+    -- Apply the dark background and theme
+    vim.opt.background = "dark"
+    vim.cmd.colorscheme("gruvbox-material")
+  end,
 }
